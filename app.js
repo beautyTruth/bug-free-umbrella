398e0393ebc81b39ee4efa7804bd354478a8b869
Phil <happy.melodist@gmail.com>
2021-12-22 23:50:27 -0600

    complete project before debugging error in classList add

--- app.js
+++ app.js
@@ -36,3 +36,34 @@
 /*
 my code below
 */
+
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+
+const formInputValidator = /^(?=\D{8})(?=\D*\{2})/;
+
+usernameInput.addEventListener("input", validate);
+passwordInput.addEventListener("input", validate);
+
+function validate(e) {
+  if (e.target.id === "username") {
+    if (e.target.value.length > 3) {
+      e.target.classlist.add("valid");
+      e.target.classlist.remove("invalid");
+    } else {
+      e.target.classlist.add("invalid");
+      e.target.classlist.remove("valid");
+    }
+  }
+
+  if (e.target.id === "password") {
+    if (formInputValidator.test(e.target.value)) {
+      e.target.classlist.add("valid");
+      e.target.classlist.remove("invalid");
+    } else {
+      e.target.classlist.add("invalid");
+      e.target.classlist.remove("valid");
+    }
+    console.log(e.target.value);
+  }
+}
